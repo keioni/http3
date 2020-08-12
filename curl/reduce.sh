f0d61ecb00381702271ba2a5a92643b86560ab27
keioni <otegami@devel.keys.jp>
2020-08-12 09:59:56 +0900

    fix minor bug

--- curl/reduce.sh
+++ curl/reduce.sh
@@ -6,12 +6,8 @@ LOCAL_DIR="/usr/local"
 BIN_DIR="$LOCAL_DIR/bin"
 LIB_DIR="$LOCAL_DIR/lib"
 
-rm -f $BIN_DIR/openssl
-rm -f $BIN_DIR/c_rehash
-rm -f $BIN_DIR/curl-config
-rm -f $LIB_DIR/*.a
-rm -f $LIB_DIR/*.la
-rm -rf $LIB_DIR/pkgconfig
+rm -rf $BIN_DIR/openssl $BIN_DIR/c_rehash $BIN_DIR/curl-config
+rm -rf $LIB_DIR/*.a $LIB_DIR/*.la $LIB_DIR/pkgconfig
 
 apk add --no-cache binutils
 find $LOCAL_DIR -type f -executable | xargs strip
@@ -20,13 +16,12 @@ apk del --no-network binutils
 rm /lib/libcrypto.so.1.1
 ln -sf $LIB_DIR/libcrypto.so.1.1 /lib
 ln -sf $LIB_DIR/libcrypto.so.1.1 /usr/lib
+
 rm /lib/libssl.so.1.1
 ln -sf $LIB_DIR/libssl.so.1.1 /lib
 ln -sf $LIB_DIR/libssl.so.1.1 /usr/lib
+
 rm -rf /usr/lib/engines-1.1
 ln -sf $LIB_DIR/engines-1.1 /usr/lib
-
-apk add nghttp2-libs
-apk del musl-utils libc-utils
 
 rm -rf /usr/share $LOCAL_DIR/share
